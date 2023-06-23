33f47e5886d0c71df4dff4ac0f96657919fce64e
saurav sharma <sauravsharma@sauravs-MacBook-Air.local>
2023-06-23 20:29:59 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cluster = require("cluster");
 const port = process.env.PORT || 4000;
 const os = require("os")
 const totalcpus = os.cpus().length;
-console.log(totalcpus);
+
 if (cluster.isPrimary) {
     for (let i = 0; i < totalcpus; i++) {
         cluster.fork()
@@ -10,10 +10,45 @@ if (cluster.isPrimary) {
 } else {
     const express = require("express");
     const server = express();
-    server.get("/", (req, res) => {
-        return res.json({ message: "ok" })
+    const morgan = require("morgan")
+    const fs = require("fs")
+    const moment = require('moment-timezone');
+    const path = require('path');
+    const expressip = require('express-ip');
+    const compression = require("compression")
+    const https = require("https")
+    server.use(compression())
+    server.use(expressip().getIpInfoMiddleware);
+
+
+    console.log(path.resolve(__dirname, "certificate", "key.pem"));
+
+    // Create a write stream (in append mode)
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+    // Custom Token Definition
+    morgan.token('date', (req, res) => {
+        return moment().tz('Asia/Kolkata').format();
+    });
+    morgan.token("ip", (req, res) => {
+
+        return req.ipInfo.ip
     })
-    server.listen(port, () => {
+    // Custom Log Format Definition
+    morgan.format('myformat', '[:date [Asia/Kolkata]] ":method :url" :status :res[content-length] :response-time ms  :ip ipAdress ');
+
+    // Apply the Custom Log Format
+    server.use(morgan('myformat', { stream: accessLogStream }));
+
+    server.get("/", (req, res) => {
+        res.json({ ok: "ok" })
+
+    })
+    const sslserver = https.createServer({
+        key: fs.readFileSync(path.resolve(__dirname, "certificate", "key.pem")),
+        cert: fs.readFileSync(path.resolve(__dirname, "certificate", "cert.pem"))
+    }, server)
+    sslserver.listen(port, () => {
         console.log("server listen on port", port);
     })
 }
